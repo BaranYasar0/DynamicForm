402ef0af9f07966c056b3a2cd91d74e191c9705a
barra <barra@DESKTOP-O75924M>
2023-04-20 16:48:06 +0300

    Add project files.

---
+++ src/Api/WebMVCApi/DynamicForm.Api.MVCApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DynamicForm.Api.MVCApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
