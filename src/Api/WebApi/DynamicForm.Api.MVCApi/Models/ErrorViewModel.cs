5909c4abdb3c3fa0974bc3fadd0d90d243798760
Ömer Baran Yaşar <1900001226@stu.iku.edu.tr>
2023-04-23 13:40:53 +0300

    Correction

--- src/Api/WebApi/DynamicForm.Api.MVCApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DynamicForm.Api.MVCApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
